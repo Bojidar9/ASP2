6b0d4cf19cc02a7d8c386aa0b42f13a20fe5efab
Bojidar9 <b.nikolov@unibit.bg>
2017-03-13 14:29:08 +0200

    added 2 new classes

---
+++ App_Code/SQLQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SQLQuery
+/// </summary>
+public class SQLQuery
+{
+    /// <summary>
+    /// Insert into news
+    /// </summary>
+    public const string NewsInsert = "Insert into news(Title, Author, Conten, CreateDate, Image) VALUES (@Title, @Author, @Conten, @CreateDate, @Image) ";
+
+    public const string InsertActivetionCode = "INSERT INTO UserActivetions VALUES (@UserId, @ActivationCode)";
+
+}
